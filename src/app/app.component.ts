c5d39c19d0f6f95c48ad98b90c297458738ec8f3
Ronaldo Sampaio <ronaldogospel@gmail.com>
2025-06-24 17:56:35 -0300

    subida landing-page venda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderLandingPageComponent } from './landing-page/header-landing-page/header-landing-page.component';
+import { FooterLandingPageComponent } from './landing-page/footer-landing-page/footer-landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLandingPageComponent,LandingPageComponent,FooterLandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'product';
+ 
 }
---
+++ src/app/landing-page/header-landing-page/header-landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-landing-page',
+  imports: [],
+  templateUrl: './header-landing-page.component.html',
+  styleUrl: './header-landing-page.component.scss'
+})
+export class HeaderLandingPageComponent {
+
+}
